b89a52cb14f1d0e88ac79dd1320afbde0f19b6ac
MarinaA2015 <m.abrashkina2015@gmail.com>
2020-07-03 08:43:48 +0300

    WorkWithFrame: comments were added

--- src/tests/WorkWithFrame.java
+++ src/tests/WorkWithFrame.java
@@ -21,13 +21,23 @@ public class WorkWithFrame {
     public void testFrames(){
         //System.out.println("Text in header:" + driver.findElement(By.xpath("//h1")).getText());
         System.out.println("Button Run outside frames:" + driver.findElement(By.xpath("//button[contains(text(),'Run')]")).getText());
+
+        //-----------Switch to frame1 ---------
         driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
         System.out.println("Text in header h2:" + driver.findElement(By.xpath("//h2")).getText());
+
+        //-----------Switch to default content---------
         driver.switchTo().parentFrame();
         System.out.println("Button Run outside frames:" + driver.findElement(By.xpath("//button[contains(text(),'Run')]")).getText());
+
+        //-----------Switch to frame1 -------------
         driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
+
+        //-----------Switch to frame2 (inside frame1)---------
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@title = 'Iframe Example']")));
         System.out.println("Text in header h1:" + driver.findElement(By.xpath("//h1")).getText());
+
+        //-----------Switch to default content---------
         driver.switchTo().defaultContent();
         System.out.println("Button Run outside frames:" + driver.findElement(By.xpath("//button[contains(text(),'Run')]")).getText());
 
